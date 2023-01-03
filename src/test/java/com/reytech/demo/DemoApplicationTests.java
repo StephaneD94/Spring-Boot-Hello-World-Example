5baca8c7e123718e282690bbd5688eccd5c431f4
Formation <hedirsondelgado@gmail.com>
2023-01-03 15:49:24 +0100

    1

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -1,6 +1,9 @@
 package com.reytech.demo;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.reytech.demo.Personne;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +12,12 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+    void groupedAssertions() {
+    Personne pers = new Personne("Nassur", "Moumadi");
 
+    assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+                          () -> assertEquals("Moumadi", pers.getLastName()));
+	}
 }
